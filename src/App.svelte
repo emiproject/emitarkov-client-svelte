d1518e3142a6f4771ff99f695c488f14ecc46ef9
kiobu <cdolson30@gmail.com>
2021-11-11 22:43:48 -0500

    Some styling and formatting

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,36 @@
 <script lang="ts">
-	export let name: string;
+	export let page: string;
+	import Navbar from "./Navbar.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<body>
+	<Navbar></Navbar>
+
+	<main>
+		<h1>{page}</h1>
+		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	</main>
+</body>
+
 
 <style>
+	*,
+	html {
+		margin: 0;
+		padding: 0;
+	}
+
+	body {
+		font-family: 'Chakra Petch';
+	}
 	main {
-		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
+		color: #ff003b;
 		font-size: 4em;
 		font-weight: 100;
 	}
@@ -26,5 +39,33 @@
 		main {
 			max-width: none;
 		}
+	}
+
+	/* azeret-mono-regular - latin */
+	@font-face {
+		font-family: 'Azeret Mono';
+		font-style: normal;
+		font-weight: 400;
+		src: url('../fonts/azeret-mono-v1-latin-regular.eot'); /* IE9 Compat Modes */
+		src: local(''),
+				url('../fonts/azeret-mono-v1-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+				url('../fonts/azeret-mono-v1-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+				url('../fonts/azeret-mono-v1-latin-regular.woff') format('woff'), /* Modern Browsers */
+				url('../fonts/azeret-mono-v1-latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
+				url('../fonts/azeret-mono-v1-latin-regular.svg#AzeretMono') format('svg'); /* Legacy iOS */
+	}
+
+		/* chakra-petch-regular - latin */
+	@font-face {
+		font-family: 'Chakra Petch';
+		font-style: normal;
+		font-weight: 400;
+		src: url('../fonts/chakra-petch-v4-latin-regular.eot'); /* IE9 Compat Modes */
+		src: local(''),
+				url('../fonts/chakra-petch-v4-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+				url('../fonts/chakra-petch-v4-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+				url('../fonts/chakra-petch-v4-latin-regular.woff') format('woff'), /* Modern Browsers */
+				url('../fonts/chakra-petch-v4-latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
+				url('../fonts/chakra-petch-v4-latin-regular.svg#ChakraPetch') format('svg'); /* Legacy iOS */
 	}
 </style>
---
+++ src/Navbar.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+  function exit(): void {
+    window.close();
+  }
+</script>
+
+<navbar>
+  <div class="nav">
+
+    <div class="title">
+      <h1>EMI Client</h1>
+    </div>
+  
+    <div class="navigation">
+      <a href="/connect/">Connect</a>
+      <a href="/settings/">Settings</a>
+      <a href="/data/">Data</a> &nbsp; &nbsp; &nbsp; &nbsp; 
+      <button class="button-dark" on:click={exit}>Exit</button>
+    </div>
+  
+  </div>
+</navbar>
+
+<style lang="scss">
+.nav {
+  padding-left: 1em;
+  padding-right: 1em;
+  border-bottom: 1px solid black;
+  padding-bottom: .5em;
+}
+
+h1 {
+  color: #ff003b;
+}
+
+a {
+  color: black;
+  border: 1px solid black;
+  padding: 3px 7px 3px 7px;
+  border-radius: 5px;
+}
+
+a:hover {
+  color: #ff003b;
+  border: 1px solid #ff003b;
+}
+
+button {
+  @extend a;
+  background-color: transparent;
+}
+
+.nav {
+  display: flex;
+  height: 50px;      /* optional; just for demo */
+  background: white;
+}
+
+.title {
+  flex: 1;          /* shorthand for: flex-grow: 1, flex-shrink: 1, flex-basis: 0 */
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.navigation {
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  a {
+    margin: 0 5px;
+    text-decoration: none;
+  }
+}
+
+/*.header-title {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #ff003b;
+}*/
+</style>
